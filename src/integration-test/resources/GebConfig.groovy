0413bb1d475a6ad0fb498bd742227b5f07dd79c2
Mark Cottman-Fields <mark.cottmanfields@qcif.edu.au>
2024-01-31 15:52:18 +1000

    update selenium test config

--- src/integration-test/resources/GebConfig.groovy
+++ src/integration-test/resources/GebConfig.groovy
@@ -1,25 +1,53 @@
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
+
+/*
+ * This file configures the geb tests for chrome and firefox.
+ * The integrationTest gradle task allows running with only one driver at time.
+ *
+ * The path to the drivers is set in build.gradle.
+ */
 
 environments {
 
-    // run via “./gradlew -Dgeb.env=chrome -Dwebdriver.chrome.driver=/Applications/chromedriver iT”
-    chrome {
-        driver = { new ChromeDriver() }
-    }
+    // --- Chrome / Chromium ---
 
-    // run via “./gradlew -Dgeb.env=chromeHeadless -Dwebdriver.chrome.driver=/Applications/chromedriver iT”
-    chromeHeadless {
+    // run using: ./gradlew -Dgeb.env=chrome iT
+    chrome {
         driver = {
             ChromeOptions o = new ChromeOptions()
-            o.addArguments('headless')
+            o.addArguments("--remote-allow-origins=*")
             new ChromeDriver(o)
         }
     }
 
-    // run via “./gradlew -Dgeb.env=htmlUnit iT”
-    htmlUnit {
-        driver = { new HtmlUnitDriver() }
+    // run using: ./gradlew -Dgeb.env=chromeHeadless iT
+    chromeHeadless {
+        driver = {
+            ChromeOptions o = new ChromeOptions()
+            o.addArguments('headless')
+            o.addArguments("window-size=1920,1080")
+            o.addArguments('--disable-dev-shm-usage')
+            o.addArguments("--remote-allow-origins=*")
+            new ChromeDriver(o)
+        }
+    }
+
+    // --- FireFox ---
+
+    // run using: ./gradlew -Dgeb.env=firefox iT
+    firefox {
+        driver = { new FirefoxDriver() }
+    }
+
+    // run using: ./gradlew -Dgeb.env=firefoxHeadless iT
+    firefoxHeadless {
+        driver = {
+            FirefoxOptions o = new FirefoxOptions()
+            o.addArguments('-headless')
+            new FirefoxDriver(o)
+        }
     }
 }
